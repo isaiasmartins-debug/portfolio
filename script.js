bf6ffc2456f5fd5595a395ca3698e8cbbe3f68cb
Isaias Oliveira <isaiasoliveiramartins12@gmail.com>
2026-01-04 17:48:39 -0300

    Ajustes no layout e tema

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 console.log("Portfólio carregado com sucesso!");
 
-// ===== Persistir modo escuro =====
-const toggleButton = document.getElementById('toggle-theme');
+const btn = document.getElementById("toggle-theme");
 
-// Aplica o tema salvo ao carregar a página
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'dark') {
-  document.body.classList.add('dark');
-}
-
-// Alterna o tema ao clicar
-toggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-
-  if (document.body.classList.contains('dark')) {
-    localStorage.setItem('theme', 'dark');
-  } else {
-    localStorage.setItem('theme', 'light');
-  }
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 });
